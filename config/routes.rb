a896d4fee3d1d92ce63060b8f8cfb407b94cccbe
Angela2389 <angela_2389@hotmail.com>
2016-09-12 15:46:28 +0200

    made logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  # In case of using the user controller:
+  #  devise_for :users, controllers: {
+  #    sessions: 'users/sessions'
+  #  }
+ # end
+  root 'categories#index'
+
   resources :categories
   get 'products/rackets' => "products#rackets"
   get 'products/shuttles' => "products#shuttles"
   get 'products/clothes' => "products#clothes"
-
   resources :products
-
-  root 'categories#index'
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
