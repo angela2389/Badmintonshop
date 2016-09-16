f88d18eb349094f4d50cb19dd11c69d41f2fefe9
Angela2389 <angela_2389@hotmail.com>
2016-09-16 12:31:46 +0200

    integration test updated

--- spec/feature/checkout_cart_spec.rb
+++ spec/feature/checkout_cart_spec.rb
@@ -13,6 +13,14 @@ feature 'Add item to cart and create an order', js: true do
     expect(page).to have_content('Yonex X')
   end
 
+  scenario 'Go to rackets page and show cart as guest' do
+    create_category_and_product
+    visit products_rackets_path
+    expect(page).to have_content('Yonex X')
+    click_link("Add to cart")
+    expect(page).to have_content('Yonex X')
+  end
+
   scenario 'Add product to cart and go to checkout' do
     user = User.create(email: "angela@angela.com", password: "abcdef")
     create_category_and_product
@@ -24,12 +32,15 @@ feature 'Add item to cart and create an order', js: true do
     expect(page).to have_content('Yonex X')
     click_link("Add to cart")
     click_link("Checkout")
-    click_link("Continue checkout")
+    fill_in 'order_deliveryaddress', with: 'De Brik 1, 1626HR Hoorn'
+    page.execute_script("$('form').submit()")
     choose("order_paymentmethod_bank_transfer")
+    page.execute_script("$('form').submit()")
     fill_in 'order_comments', with: 'quick delivery!!'
     page.execute_script("$('form').submit()")
     sleep(3)
     expect(page).to have_content('Order id: 1')
     expect(page).to have_content('quick delivery!!')
+    expect(page).to have_content('Yonex X')
   end
 end
